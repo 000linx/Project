c4946b9c36ca3f8b34b6b40e75e6c57885a6e512
linx <1405128011@qq.com>
2024-12-18 15:30:53 +0800

    修改了一些接口

--- stu_module.py
+++ stu_module.py
@@ -21,23 +21,24 @@ stu_bp = Blueprint('stu', __name__)
 def login():
     data = request.get_json()
     if data is None:
-        return jsonify({'message': '请求参数出错'}), 400
+        raise Exception('请求参数错误')
     try:
         stuid = data['stuid']
         password = data['pwd']
+        if stuid is None or password is None:
+            raise Exception('账号和密码不能为空')
         student = stu_collcetion.find_one({'stuid': stuid})
-        print(student)
         if student is not None:
             if check_password_hash(student['password'], password):
                 access_token = create_access_token(identity=generate_identity(stuid))
-
                 return jsonify({'message': '登录成功', 'access_token': access_token}), 200
             else:
-                return jsonify({'message': '密码错误'}), 400
+                raise Exception('密码错误') 
         else:
-            return jsonify({'message': '当前学生不存在'}), 400
+            raise Exception('用户不存在') 
     except Exception as e:
-        return jsonify({'message': '登录异常'}), 400
+        return jsonify({"发生异常":str(e)}), 400
+
 
 # 获取个人信息
 @stu_bp.route('/StuInfo', methods=['POST'])
@@ -47,17 +48,142 @@ def get_info():
     data = request.get_json()
     try:   
         stuid = data['stuid']
+        if stuid is None:
+            raise Exception('学号为空')
         if check_identity(current_user, stuid) == False:
-            return jsonify({'message': '用户错误'}), 400
+           raise Exception('用户错误') 
         stu = stu_collcetion.find_one({'stuid': stuid},{filed: 0 for filed in excluded_fields})
         if stu is None:
-            return jsonify({'message': '当前用户不存在'}), 400
+            raise Exception('用户不存在')
         stu_data = {'faculty':stu['faculty'],'name': stu['name'],'stuid': stu['stuid'], 'phone': stu['phone'], 'className': stu['className'], 'dormitoryid': stu['dormitoryid']}
-        stu_bp.logger.info(f'用户{stuid}获取个人信息成功')
         return jsonify({'message': '获取成功','stu_data':stu_data}), 200
     except Exception as e:
-        stu_bp.logger.error(e)
-        return jsonify({'message': '获取异常'}), 400
+        return jsonify({"发生异常":str(e)}), 400
+    
+    
+# 修改个人信息
+@stu_bp.route('/StuUpdate', methods=['POST'])
+@jwt_required()
+def update_info():
+    try:
+        data = request.get_json()
+        stuid = data['stuid']
+        phone = data['phone']
+        className = data['className']
+        dormitoryid = data['dormitoryid']
+        current_user = get_jwt_identity()
+        if check_identity(current_user, stuid) == False:
+            raise Exception('当前用户错误') from e
+        if stuid is None:
+            raise Exception('用户不存在') from e
+        stu_collcetion.update_one({'stuid': stuid}, {'$set': { 'phone': phone, 'className': className, 'dormitoryid': dormitoryid}})
+        stu = stu_collcetion.find_one({'stuid': stuid},{filed: 0 for filed in excluded_fields})
+        stu_data = {'faculty':stu['faculty'],'name': stu['name'], 'stuid': stu['stuid'], 'phone': stu['phone'], 'className': stu['className'], 'dormitoryid': stu['dormitoryid']}
+        return jsonify({'message': '修改成功','stu_data':stu_data}), 200
+    except Exception as e:
+        return jsonify({"发生异常":str(e)}), 400
+
+
+# 获取单个通知
+@stu_bp.route('/StuNotice', methods=['POST'])
+@jwt_required()
+def stu_notice():
+    try:
+        data = request.get_json()
+        stuid = data['stuid']
+        noticeid = data['noticeid']
+        if stuid is None or noticeid is None:
+            raise Exception('学号或通知id为空')
+        current_user = get_jwt_identity()
+        if check_identity(current_user, stuid) == False:
+            raise Exception('当前用户错误')
+        notice = notice_collcetion.find_one({'noticeid': noticeid},{'_id':0})
+        if notice is None:
+            raise Exception('当前通知不存在')
+        return jsonify({'message': '获取成功','notice':notice}), 200
+    except Exception as e:
+        return jsonify({"发生异常":str(e)}), 400
+
+
+# 获取全部通知
+@stu_bp.route('/StuAllNotices', methods=['POST'])
+@jwt_required()
+def get_notice():
+    try:
+        data = request.get_json()
+        stuid = data['stuid']
+        current_user = get_jwt_identity()
+        if check_identity(current_user, stuid) == False:
+            raise Exception('当前用户错误')
+        notices = notice_collcetion.find({},{'_id':0})
+        if notices is None:
+            return jsonify({'message': '当前无通知'}), 200
+        notices_list = []
+        for notice in notices:
+            notices_list.append(notice)
+        return jsonify({'message': '获取成功','notices':notices_list}), 200
+    except Exception as e:
+        return jsonify({"发生异常":str(e)}), 400
+
+# 获取单个报修记录
+@stu_bp.route('/StuRepair', methods=['POST'])
+@jwt_required()
+def get_repaire():
+    try:
+        data  = request.get_json()
+        stuid = data['stuid']
+        repairid = data['repairid']
+        if stuid is None or repairid is None:
+            raise Exception('学号或报修id为空')
+        current_user = get_jwt_identity()
+        if check_identity(current_user, stuid) == False:
+            raise Exception('当前用户错误')
+        repair = repair_collection.find_one({'repairid': repairid, "stuid": stuid},{'_id':0})
+        if repair is None:
+            raise Exception('当前报修记录不存在')
+        return jsonify({'message': '获取成功','repair':repair}), 200
+    except Exception as e:
+        return jsonify({"发生异常":str(e)}), 400
+
+# 获取全部报修记录
+@stu_bp.route('/StuRepairs', methods=['POST'])
+@jwt_required()
+def get_repairs():
+    try:
+        data = request.get_json()
+        stuid = data['stuid']
+        current_user = get_jwt_identity()
+        if check_identity(current_user, stuid) == False:
+            raise Exception('当前用户错误')
+        repairs = repair_collection.find({'stuid': stuid},{'_id':0})
+        if repairs is None:
+            return jsonify({'message': '当前无报修记录'}), 200
+        repairs_list = []
+        for repair in repairs:
+            repairs_list.append(repair)
+        return jsonify({'message': '获取成功','repaires':repairs_list}), 200
+    except Exception as e:
+        return jsonify({"发生异常":str(e)}), 400
+
+# 宿舍信息
+@stu_bp.route('/StuDormitory', methods=['POST'])
+@jwt_required()
+def get_dormitory():
+    try:
+        data = request.get_json()
+        stuid = data['stuid']
+        dormitoryid = data['dormitoryid']
+        if stuid is None or dormitoryid is None:
+            raise Exception('学号或宿舍id为空')
+        current_user = get_jwt_identity()
+        if check_identity(current_user, stuid) == False:
+            return jsonify({'message': '用户错误'}), 400
+        dormitory = dormitory_collection.find_one({'dormitoryid':dormitoryid},{'_id':0})
+        if dormitory is None:
+            raise Exception('当前宿舍不存在')
+        return jsonify({'message': '获取成功','dormitory':dormitory}), 200
+    except Exception as e:
+        return jsonify({"发生异常":str(e)}), 400
 
 # 修改密码
 @stu_bp.route('/StuChangePwd', methods=['POST'])
@@ -75,91 +201,3 @@ def change_pwd():
         pass
     else:
         return jsonify({'message':'用户不存在'}), 400
-    
-
-
-
-# 修改个人信息
-@stu_bp.route('/StuUpdate', methods=['POST'])
-@jwt_required()
-def update_info():
-    data = request.get_json()
-    stuid = data['stuid']
-    phone = data['phone']
-    className = data['className']
-    dormitoryid = data['dormitoryid']
-    current_user = get_jwt_identity()
-    if check_identity(current_user, stuid) == False:
-        return jsonify({'message': '用户错误'}), 400
-    if stuid is None:
-        return jsonify({'message': '当前用户不存在'}), 400
-    stu_collcetion.update_one({'stuid': stuid}, {'$set': { 'phone': phone, 'className': className, 'dormitoryid': dormitoryid}})
-    stu = stu_collcetion.find_one({'stuid': stuid},{filed: 0 for filed in excluded_fields})
-    stu_data = {'faculty':stu['faculty'],'name': stu['name'], 'stuid': stu['stuid'], 'phone': stu['phone'], 'className': stu['className'], 'dormitoryid': stu['dormitoryid']}
-    return jsonify({'message': '修改成功','stu_data':stu_data}), 200
-
-# 获取单个通知
-@stu_bp.route('/StuNotice', methods=['POST'])
-@jwt_required()
-def stu_notice():
-    data = request.get_json()
-    noticeid = data['noticeid']
-    notice = notice_collcetion.find_one({'noticeid': noticeid},{'_id':0})
-    if notice is None:
-        return jsonify({'message': '当前通知不存在'}), 400
-    return jsonify({'message': '获取成功','notice':notice}), 200
-
-# 获取全部通知
-@stu_bp.route('/StuNotices', methods=['GET'])
-@jwt_required()
-def get_notice():
-    notices = notice_collcetion.find({},{'_id':0})
-    notice_list = []
-    for notice in notices:
-        notice_list.append(notice)
-    return jsonify({'message': '获取成功','notice_list':notice_list}), 200
-
-# 获取单个报修记录
-@stu_bp.route('/StuRepaire', methods=['POST'])
-@jwt_required()
-def get_repaire():
-    data = request.get_json()
-    stuid = data['stuid']
-    current_user = get_jwt_identity()
-    if check_identity(current_user, stuid) == False:
-        return jsonify({'message': '用户错误'}), 400
-    repairid = data['repairid']
-    repair = repair_collection.find_one({'stuid': stuid, 'repairid': repairid},{'_id':0})
-    if repair is None:
-        return jsonify({'message': '当前报修记录不存在'}), 400
-    return jsonify({'message': '获取成功','repair':repair}), 200
-
-# 获取全部报修记录
-@stu_bp.route('/StuRepaires', methods=['POST'])
-@jwt_required()
-def get_repairs():
-    data = request.get_json()
-    stuid = data['stuid']
-    current_user = get_jwt_identity()
-    if check_identity(current_user, stuid) == False:
-        return jsonify({'message': '用户错误'}), 400
-    repairs = repair_collection.find({'stuid': stuid},{'_id':0})
-    repairs_list = []
-    for repair in repairs:
-        repairs_list.append(repair)
-    return jsonify({'message': '获取成功',"repairs":repairs_list}), 200
-
-# 宿舍信息
-@stu_bp.route('/StuDormitory', methods=['POST'])
-@jwt_required()
-def get_dormitory():
-    data = request.get_json()
-    stuid = data['stuid']
-    dormitoryid = data['dormitoryid']
-    current_user = get_jwt_identity()
-    if check_identity(current_user, stuid) == False:
-        return jsonify({'message': '用户错误'}), 400
-    dormitory = dormitory_collection.find_one({'dormitoryid':dormitoryid},{'_id':0})
-    if dormitory is None:
-        return jsonify({'message': '当前宿舍不存在'}), 400
-    return jsonify({'message': '获取成功','dormitory':dormitory}), 200
--- utils.py
+++ utils.py
@@ -2,10 +2,12 @@ import random
 import string
 from werkzeug.security import generate_password_hash, check_password_hash
 
+# 生成认证码
 def generate_identity(id):
     identity = generate_password_hash(id)
     return ''.join(identity)
 
+# 检查认证码
 def check_identity(identity, id):
     return check_password_hash(identity, id)
 
